9510430e8a6729c9ab218eefce80d7c36a9da33e
Garibaldini85 <garibaldini27185285@gmail.com>
2025-07-28 19:57:18 +0200

    Добавление маршрута для отображения меню. Добавление маршрута для обслуживания статических файлов. Перегрузка переменной handler404 для отображения собственного представления ошибки 404. Подключение маршрута для обслуживания статических файлов из STATIC_ROOT, когда DEBUG = False.

--- DjangoTreeMenu/urls.py
+++ DjangoTreeMenu/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
+from django.conf.urls import handler404
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+
+from tree_menu.views import test_menu_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test_menu_view, name='home'),
+    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+
+handler404 = 'tree_menu.views.custom_404_view'
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
