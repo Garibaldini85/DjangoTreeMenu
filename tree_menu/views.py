5749401a4e18d6af8ceb94691b1d9a9757d6339c
Garibaldini85 <garibaldini27185285@gmail.com>
2025-07-28 19:52:47 +0200

    Добавление представления test_menu_view для отображения меню в тестовом варианте. Добавление представления custom_404_view для отображения ошибки 404.

--- tree_menu/views.py
+++ tree_menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def test_menu_view(request):
+    return render(request, 'base.html')
+
+
+def custom_404_view(request, exception):
+    return render(request, 'errors/404.html', status=404)
