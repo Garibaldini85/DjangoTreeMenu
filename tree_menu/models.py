881f90187f3582bcbd46a74a940b59fd53da7a2a
Garibaldini85 <garibaldini27185285@gmail.com>
2025-07-28 18:55:03 +0200

    Создание моделей Меню и Пунктов Меню

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import NoReverseMatch, reverse
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100, blank=True, help_text='Путь или name URL')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def get_absolute_url(self):
+        try:
+            return reverse(self.url)
+        except NoReverseMatch:
+            return self.url
+
+    def __str__(self):
+        return self.name
