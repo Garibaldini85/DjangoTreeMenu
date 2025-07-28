73d734d391577ead9822df1e4cde3f8f6b7d201c
Garibaldini85 <garibaldini27185285@gmail.com>
2025-07-28 20:06:04 +0200

    Переключение на режим выпуска продукта для правильной работы хендлера 404 ошибки. Определяем список разрешенных адресов для доступа к сервису. Добавляем приложение tree_menu, которое реализует логику с древовидным меню. Определение STATIC_ROOT, папки, в которой хранятся все статические файлы.

--- DjangoTreeMenu/settings.py
+++ DjangoTreeMenu/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f&vwij-)ukd=h#(3%vd=ty*c%$t6r9!36stv!+wxulvu94#*h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tree_menu',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
